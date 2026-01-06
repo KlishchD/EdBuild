b65cb251b096498ae40d45982627ad2133f667c3
KlishchD <klishch.dmytro@gmail.com>
2026-01-06 22:05:52 +0100

    Added platform utils.

---
+++ estd/platform.h
@@ -0,0 +1,15 @@
+#pragma once
+
+namespace estd
+{
+  std::string fetch_executable_path()
+  {
+    constexpr std::size_t buffer_size = MAX_PATH;
+    char executable_path[buffer_size];
+
+    std::size_t size = GetModuleFileNameA(nullptr, executable_path, buffer_size);
+    estd::assert_condition(size, "Failed to fetch executable path.");
+
+    return executable_path;
+  }
+}
